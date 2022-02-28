b4af19e600ef9dbb91717a282ca80841db897816
Myozarniaung <myozarniaung1990@gmail.com>
2022-02-28 21:37:52 +0800

    Add files via upload

---
+++ cartapi/Models/cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace cartapi.Models
+{
+    public class cart
+    {
+        public int cartId { get; set; }
+        public int productId { get; set; }
+        public double price { get; set; }
+        public int total { get; set; }
+        public string orderId { get; set; }
+        public string Status { get; set; }
+    }
+}
